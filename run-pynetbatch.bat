a92d4b39f35989f2b2f1f0de0613ba94b5ea6041
AlainCo <13969182+AlainCo@users.noreply.github.com>
2025-05-22 10:51:18 +0200

    traces

--- run-pynetbatch.bat
+++ run-pynetbatch.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-start /min %~dp0dist\pynetwatch.exe --config-file=%~dp0pynetwatch-config.json --config-create=true
+start /min %~dp0dist\pynetwatch.exe --config-file=%~dp0pynetwatch-config.json --config-create=true --log-file=%~dp0pynetwatch.log
