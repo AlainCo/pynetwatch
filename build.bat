7fbd1ff9e00f866133c55eab423cc8d9ecd79545
AlainCo <13969182+AlainCo@users.noreply.github.com>
2025-05-29 15:10:59 +0200

    improve scripts prevent reset venv

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 @echo off
+setlocal
 call .venv\Scripts\activate
 pyinstaller --windowed --onefile --clean --add-data "icons/*.ico;icons/" --name pyNetWatch src\main.pyw
 call .venv\Scripts\deactivate
+endlocal
